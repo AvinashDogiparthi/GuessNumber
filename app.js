0face74b617ce1a628b699b04608626a24a9304d
Avinash Dogiparthi <79859093+AvinashDogiparthi@users.noreply.github.com>
2023-06-05 11:32:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let maximum = parseInt(prompt("Enter the maximum number !"));
+while(!maximum)
+{
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while(guess !== targetNum)
+{
+    attempts += 1
+    if(guess > targetNum)
+    {
+        guess = parseInt(prompt("too high! Enter a new guess"));
+    }
+    else{
+        guess = parseInt(prompt("too low! Enter a new guess"));
+    }
+}
+console.log(`You got it, it took you ${attempts} guesses`);
